a4b632fc28dc0fcc1622b002edf775e46088631d
Sergey Katin <katinsv@profi.ru>
2017-01-11 09:04:30 +0300

    Purchasing podcasts

--- app/grab.php
+++ app/grab.php
@@ -3,24 +3,40 @@
 require_once "Esl.php";
 $config = require "configs/config.php";
 $esl = new Esl($config['login'], $config['password'], __DIR__ .'/../data');
+
 if($esl->login()) {
 
   echo "Logged in\n";
 
   $data = $esl->getData();
-  $postNames = array_map(function ($post) { return $post['href']; }, $data['posts']);
 
   $posts = $esl->grabPosts();
-  echo "Got " . count($posts) . " posts\n";
-
-  $newPosts = [];
-  foreach ($posts as $post) {
-    if( !in_array($post['href'], $postNames) ) {
-      $newPosts[] = $post;
+  $postsByID = [];
+  foreach ($posts as $k => $post) {
+    $post['id'] = preg_replace('/^.*?(\d+).*$/u', '\\1', $post['name']);
+    if(isset($data['posts'][$post['id']])) {
+      $post = array_merge($data['posts'][$post['id']], $post);
     }
+    $posts[$k] = $post;
+    $postsByID[$post['id']] = $post;
   }
-  echo "Got " . count($newPosts) . " NEW posts\n";
 
-  $esl->saveData(["posts" => $posts]);
+  $esl->saveData(["posts" => $postsByID]);
+
+  if($coupon = $esl->getCoupon()) {
+    $cnt = 0;
+    for($i = count($posts) - 1; $i >= 0 && $cnt < min($coupon['remain'], 3); $i--) {
+      if( empty($posts[$i]['purchased']) ) {
+        $links[] = $posts[$i]['href'];
+        $postsByID[$post['id']]['purchased'] = true;
+        $cnt ++;
+        echo $posts[$i]['name'] . "\n";
+      }
+    }
+    $esl->purchase($links);
+    $esl->saveData(["posts" => $postsByID]);
+    echo "done\n";
+  }
+
   echo "Data saved\n";
 }
