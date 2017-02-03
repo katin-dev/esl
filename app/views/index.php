a0572bf4ce960cd69b33dc5bb3146f89bcb77627
Sergey Katin <katin.dev@gmail.com>
2017-02-03 21:45:44 +0300

    New index page

--- app/views/index.php
+++ app/views/index.php
@@ -8,12 +8,32 @@
             list-style: none;
         }
     </style>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <style>
+
+    </style>
 </head>
 <body>
-    <ul>
-    <?php foreach($posts as $post): ?>
-      <li><a href="<?=$post['url']?>"><?=$this->e($post['name'])?></a></li>
-    <?php endforeach; ?>
-    </ul>
+    <div class="container">
+        <h1>ESL podcasts</h1>
+        <ul class="list-unstyled">
+          <?php foreach($posts as $post): ?>
+              <li>
+                  <a href="<?=$post['url']?>"><?=$this->e($post['name'])?></a>
+                  <?php if(isset($post['files']['mp3'])): ?>
+                      <a href="/podcasts/<?=$post['files']['mp3']['filename']?>"><span class="glyphicon glyphicon-volume-up"></span></a>
+                  <?php endif; ?>
+                  <?php if(isset($post['files']['pdf'])): ?>
+                      <a href="/podcasts/<?=$post['files']['pdf']['filename']?>"><span class="glyphicon glyphicon-book"></span></a>
+                  <?php endif; ?>
+              </li>
+          <?php endforeach; ?>
+        </ul>
+    </div>
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -7,6 +7,23 @@ $app->get('/', function () use ($app) {
   $db = $app['db'];
   $stmt = $db->query("SELECT * FROM podcast ORDER BY id DESC");
   $podcasts = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+  $stmt = $db->query("SELECT podcast_id, type, filename FROM file");
+  $files = $stmt->fetchAll(\PDO::FETCH_ASSOC | \PDO::FETCH_GROUP);
+
+  foreach ($podcasts as $key => $podcast) {
+    if(isset($files[$podcast['id']])) {
+      foreach ($files[$podcast['id']] as $file) {
+        if($file['type'] == 'pdf') {
+          $podcasts[$key]['files']['pdf'] = $file;
+        }
+        if($file['type'] == 'mp3') {
+          $podcasts[$key]['files']['mp3'] = $file;
+        }
+      }
+    }
+  }
+
   return $app['view']->render("index", ['posts' => $podcasts]);
 });
 
